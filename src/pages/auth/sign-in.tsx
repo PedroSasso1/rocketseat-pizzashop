f66e9603340717bed48af7ff5751ed847aae0002
pedrosasso1 <pedrosassom@outlook.com>
2024-07-14 16:37:09 -0300

    sign in and sign up pages

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,15 +1,51 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
+const signInFormSchema = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInFormSchema>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({
+    resolver: zodResolver(signInFormSchema),
+  })
+
+  async function handleSignIn(data: SignInForm) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      toast.success('Enviamos um link de autenticação para seu e-mail!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (error) {
+      toast.error('Erro ao enviar link de autenticação!')
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
         <div className="flex w-[350px] flex-col justify-center gap-6">
+          <Button asChild className="absolute right-8 top-8" variant="ghost">
+            <Link to="/sign-up">Novo estabelecimento</Link>
+          </Button>
           <div>
             <h1 className="text-2xl font-semibold tracking-tight">
               Acessar painel
@@ -19,13 +55,15 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full">Acessar painel</Button>
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
+              Acessar painel
+            </Button>
           </form>
         </div>
       </div>
